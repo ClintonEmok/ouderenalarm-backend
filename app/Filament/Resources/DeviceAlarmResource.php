cfe3fb073891e892652a3419ca74e40f1ef704a3
Clinton <c.emok@student.tue.nl>
2025-04-01 14:49:29 +0200

    sort on created_at

--- app/Filament/Resources/DeviceAlarmResource.php
+++ app/Filament/Resources/DeviceAlarmResource.php
@@ -81,7 +81,7 @@ class DeviceAlarmResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])->defaultSort('created_at','desc');
     }
 
     public static function getRelations(): array
