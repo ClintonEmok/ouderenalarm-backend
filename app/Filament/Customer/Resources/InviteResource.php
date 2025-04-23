3941bbc5d5dd3d71cb6aac3c6458f76ad0fe340e
Clinton <c.emok@student.tue.nl>
2025-04-23 19:14:42 +0200

    Create resource for invites

---
+++ app/Filament/Customer/Resources/InviteResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Customer\Resources;
+
+use App\Filament\Customer\Resources\InviteResource\Pages;
+use App\Filament\Customer\Resources\InviteResource\RelationManagers;
+use App\Models\Invite;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InviteResource extends Resource
+{
+    protected static ?string $model = Invite::class;
+    protected static bool $shouldRegisterNavigation = false;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInvites::route('/'),
+            'create' => Pages\CreateInvite::route('/create'),
+            'edit' => Pages\EditInvite::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Customer/Resources/InviteResource/Pages/CreateInvite.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Customer\Resources\InviteResource\Pages;
+
+use App\Filament\Customer\Resources\InviteResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateInvite extends CreateRecord
+{
+    protected static string $resource = InviteResource::class;
+}
